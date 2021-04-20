a872312e63702ba7a3135d4ecc1fbfa53545e30e
Seth Morton <sethmorton@Seths-MacBook-Air.local>
2021-04-19 23:19:35 -0700

    test commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello Mica!</h1>
+	<p>How was your day?</p>
 </main>
 
 <style>
